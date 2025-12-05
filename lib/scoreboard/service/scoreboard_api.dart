3561dd3468a3ac668633e2c4deb498aabbe7eb7d
mfadhilfajar-oss <m.fadhilfajar@gmail.com>
2025-12-05 23:11:48 +0700

    update file scoreboard_api

--- lib/scoreboard/service/scoreboard_api.dart
+++ lib/scoreboard/service/scoreboard_api.dart
@@ -1,14 +1,15 @@
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 import 'package:sportwatch_ng/scoreboard/models/scoreboard_entry.dart';
 
-class ScoreboardApi {
-  final String baseUrl = 'http://localhost:8000/scoreboard/filter/';
 
-  Future<List<ScoreboardMatch>> fetchMatches({
-    String? sport,
-  }) async {
+class ScoreboardApi {
+  late final String baseUrl = kIsWeb
+      ? 'http://127.0.0.1:8000/scoreboard/filter/'
+      : 'http://10.0.2.2:8000/scoreboard/filter/';
+
+  Future<List<ScoreboardMatch>> fetchMatches({String? sport}) async {
     final uri = Uri.parse(baseUrl).replace(
       queryParameters: {
         if (sport != null) 'sport': sport,
@@ -29,3 +30,4 @@ class ScoreboardApi {
         .toList();
   }
 }
+
